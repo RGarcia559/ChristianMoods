609333d6471183d1b56e0e1edcde1a9000614c04
Robert Garcia <robbie559@gmail.com>
2021-03-18 01:29:53 -0700

    yee

---
+++ JS verses/anger.js
@@ -0,0 +1,4 @@
+var title=document.getElementById('Anger');
+title.innerText = "Be ye angry, and sin not: let not the sun go down upon your wrath:";
+
+
---
+++ actions.js
@@ -0,0 +1,45 @@
+console.log("JavaScript is working, PRAISE GOD!")
+;
+// function list
+
+
+function callAnger() {
+    var title=document.getElementById('container')
+    title.innerText = "Be ye angry, and sin not: let not the sun go down upon your wrath: -Paul's letter to the Hebrews 4:16";
+
+}
+function callAnxiety() {
+    var title=document.getElementById('container')
+    title.innerText = "Be careful for nothing; but in every thing by prayer and supplication with thanksgiving let your requests be made known unto God.  -Paul's letter the Phillippians 4:6"
+}
+
+function callBitter() {
+    var title=document.getElementById('container')
+    title.innerText = "Let all bitterness, and wrath, and anger, and clamour, and evil speaking, be put away from you, with all malice: And be ye kind one to another, tenderhearted, forgiving one another, even as God for Christ's sake hath forgiven you  -Paul's letter the Ephesians 4:31-32"
+}
+
+function callLust() {
+    var title=document.getElementById('container')
+    title.innerText = " For this is the will of God, even your sanctification, that ye should abstain from fornication: That every one of you should know how to possess his vessel in sanctification and honour; Not in the lust of concupiscence, even as the Gentiles which know not God:.  -Paul's first letter the Thessalonians 4:3-6"
+}
+
+function callDepressed() {
+    var title=document.getElementById('container')
+    title.innerText = "And I heard a great voice out of heaven saying, Behold, the tabernacle of God is with men, and he will dwell with them, and they shall be his people, and God himself shall be with them, and be their God. And God shall wipe away all tears from their eyes; and there shall be no more death, neither sorrow, nor crying, neither shall there be any more pain: for the former things are passed away.  -The book of the Revelation of Jesus Christ 21:3-4"
+}
+
+function callProud() {
+    var title=document.getElementById('container')
+    title.innerText = "Thus saith the LORD, Let not the wise man glory in his wisdom, neither let the mighty man glory in his might, let not the rich man glory in his riches: But let him that glorieth glory in this, that he understandeth and knoweth me, that I am the LORD which exercise lovingkindness, judgment, and righteousness, in the earth: for in these things I delight, saith the LORD. Behold, the days come, saith the LORD, that I will punish all them which are circumcised with the uncircumcised; Egypt, and Judah, and Edom, and the children of Ammon, and Moab, and all that are in the utmost corners, that dwell in the wilderness: for all these nations are uncircumcised, and all the house of Israel are uncircumcised in the heart.   -The Prophet Jeremiah 9:23-26 "
+}
+
+function callSad() {
+    var title=document.getElementById('container')
+    title.innerText = "Cast thy burden upon the LORD, and he shall sustain thee: he shall never suffer the righteous to be moved.  -Psalms 55:22"
+}
+
+function callScared() {
+    var title=document.getElementById('container')
+    title.innerText = "Therefore being justified by faith, we have peace with God through our Lord Jesus Christ: By whom also we have access by faith into this grace wherein we stand, and rejoice in hope of the glory of God. And not only so, but we glory in tribulations also: knowing that tribulation worketh patience;  And patience, experience; and experience, hope: And hope maketh not ashamed; because the love of God is shed abroad in our hearts by the Holy Ghost which is given unto us. For when we were yet without strength, in due time Christ died for the ungodly. For scarcely for a righteous man will one die: yet peradventure for a good man some would even dare to die. But God commendeth his love toward us, in that, while we were yet sinners, Christ died for us. Much more then, being now justified by his blood, we shall be saved from wrath through him.   -Paul's letter the Romans 5:1-9" 
+}
+
